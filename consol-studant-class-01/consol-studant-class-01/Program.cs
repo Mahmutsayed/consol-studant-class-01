9b991968687c517b15becd4e32088d54e2e2be63
Gelisim University <bim@gelisim.edu.tr>
2026-02-20 15:57:27 +0300

    intial commet

---
+++ consol-studant-class-01/consol-studant-class-01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace consol_studant_class_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
